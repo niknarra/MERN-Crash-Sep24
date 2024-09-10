12c922da5a376e75fcb0ba0553863679b9644a7f
Nikhil Narra <nikhilnarra@vt.edu>
2024-09-10 17:39:34 -0400

    Backend Setup

---
+++ Backend/routes/product.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controller/product.controller.js";
+
+const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.send("Welcome to the Products Catalog!");
+// });
+
+router.get("/", getProducts);
+
+router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
 const app = express();
+app.use(express.json()); // Allows JSON Data in req.body -> Middleware
 const PORT = 3500;
 
+app.use("/api/products", productRoutes);
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server started at http://localhost:${PORT}`);
 });
