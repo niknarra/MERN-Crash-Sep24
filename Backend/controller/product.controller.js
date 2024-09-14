890738f22446aed8b063e0f9591430bdb57c0ee0
Nikhil Narra <nikhilnarra@vt.edu>
2024-09-14 12:15:52 -0400

    Complete Backend and Frontend init - 9/14

--- Backend/controller/product.controller.js
+++ Backend/controller/product.controller.js
@@ -50,11 +50,14 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "ID not found" });
+  }
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Deleted" });
   } catch (error) {
     console.log(`Error. ${error.message}`);
-    res.status(404);
+    res.status(500).json({ success: false, message: "Error deleting Product" });
   }
 };
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Button, Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+      </Routes>
+    </Box>
+  );
+}
+
+export default App;
