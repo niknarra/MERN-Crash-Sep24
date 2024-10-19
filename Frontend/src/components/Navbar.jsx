a03aec6c1fff247bd6670da311bd5370a8b36992
Nikhil Narra <nikhilnarra@vt.edu>
2024-10-19 18:42:04 -0400

    Complete App

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { Button, Container, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
         h={16}
         alignItems={"center"}
         justifyContent={"space-between"}
-        flexDir={{ base: "column", sm: "row" }}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
       >
         <Text
           fontSize={{ base: "22", sm: "28" }}
@@ -25,13 +40,15 @@ function Navbar() {
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
             <Button>
-              <PlusSquareIcon />
+              <PlusSquareIcon fontSize={20} />
             </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
   );
-}
-
+};
 export default Navbar;
